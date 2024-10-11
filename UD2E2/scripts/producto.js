ed5ee37f45fe7f68ab41d42e1df934bb57540daf
unknown <daw2@IESMHP.LOCAL>
2024-10-11 17:36:38 +0200

    mensaje

--- UD2E2/scripts/producto.js
+++ UD2E2/scripts/producto.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+
 let productos={};
 
     
@@ -9,24 +11,20 @@ producto si no existe. Si el producto ya existe muestra un alert indicando que e
 producto ya existe
 */
 
-
 function agregarProducto(nombre,cantidad,precio,categoria){
-
         if(nombre in productos){
-            alert('Producto');
+            alert('El producto ya existe');
+            return;
         }
-
         let producto={
             cantidad:cantidad,
             precio:precio,
             categoria:categoria,
-
         }
-
         //lo añade al array
-        producto[nombre]=producto;
+        productos[nombre]=producto;
         alert('Producto '+ nombre  + ' añadido');
-};
+}
 
 /*
 Función “eliminarProducto(nombre)” que elimina el producto del inventario. Si el
@@ -43,27 +41,28 @@ function eliminarProducto(nombre){
         alert('El producto no existe, no se pudo eliminar');
     }
 
-};
+}
 
 /*
 Función “buscarProducto(nombre)” que devuelve el objeto con la información
 del producto
 */
-function buscarProducto(nombre){
-    
-    if(nombre in productos){
-        
-        alert('El producto encontrado: ');
-        alert(
-        'Nombre '+ productos[nombre] + '\n' +
-        'Cantidad '+ productos[nombre].cantidad + '\n' +
-        'Precio '+ productos[nombre].precio + '\n' +
-        'Categoria ' + productos[nombre].categoria);
 
-    }else{
-        alert('El producto no ha sido encontrado');
+function buscarProducto(nombre) {
+    if(nombre in productos) {
+        alert(
+            'Datos del producto ' + "\n" 
+            + 'Nombre: ' + nombre + "\n" 
+            + 'Cantidad: ' + productos[nombre].cantidad + "\n"
+            + 'Precio: ' + productos[nombre].precio + "\n"
+            + 'Categoria: ' + productos[nombre].categoria + "\n"
+        );
+    } else {
+        alert('No existe ese producto');
     }
-};
+}
+
+
 /*
 Función “actualizarInventario(nombre, cantidad)” incrementa o decrementa el
 stock según la cantidad indicada. Si la reducción deja el stock a 0 se mostrará un
@@ -78,22 +77,73 @@ function actualizarInventario(nombre,cantidad){
     }else if( productos[nombre].cantidad ===0){
        alert('solicitando reposicion...')
     }
-};
+}
 /*
 Función “ordenarProductosPorPrecio()” devuelve una lista ordenada de forma
 ascendente. El resultado es un nuevo array, cuidado que no tienes el nombre.
 */
-function ordenarProductosPorPrecio(){
+function ordenarProductosPorPrecio() {
+    let datosEnArray = Object.entries(productos); //lo pasa a un array normal
+    let datosFormateados = datosEnArray.map(([nombre, valor]) => ({nombre, ...valor}));
+    //es un foreach a una coleccion nueva con un formato personalizado
+    let datosOrdenados = datosFormateados.sort((a,b) => a.precio - b.precio);
 
-    productos=array
+  
+    return datosOrdenados;
+}
+/*Función “imprimirInventario()” devuelve una lista con el informe de inventario, el
+cual muestra el “nombre”, “categoría”, “cantidad”, “precio” y “total”
+(cantidad*precio)*/
+function imprimirInventario(){
 
+    for(let nombre in productos){
+        let producto=productos[nombre];
+        alert(
+            'Nombre:' + producto.nombre,
+            'Categoria: '+ producto.categoria,
+            'Cantidad:'+ producto.cantidad,
+            'Precio:' + producto.precio,
+            'Total:'+ producto.cantidad * producto.precio
+        );
+    }
+    
+       
+    
+
+}
+
+/*filter(): Filtra el inventario y devuelve solo los productos 
+cuya categoría coincida con la categoría proporcionada.*/
+function filtrarProductosPorCategoria(categoria){
+
+    for(let nombre in productos ){
+        let producto= productos[nombre];
+        if(producto.categoria === categoria){
+            alert(
+                'Productos por categoria: '+ "\n" +
+                'Categoria: '+ producto.categoria + "\n" +
+                'Nombre del producto: '+ producto.nombre + "\n"+
+                'Cantidad '+ producto.cantidad + "\n" + 
+                'Precio ' + producto.precio + "\n"
+            );
+        }
+    }
+    
 
 }
 //funciones 
-agregarProducto('platanos',10,5.0,'fruteria');
+// agregarProducto('platanos',10,5.0,'fruteria');
+
 agregarProducto('manzanas',5,5.0,'fruteria');
+agregarProducto('platanos',5,5.0,'fruteria');
+agregarProducto('detergente',5,5.0,'limpieza');
 
-buscarProducto('platanos');
-buscarProducto('manzanas');
+// eliminarProducto('manzanas');
 
-eliminarProducto('platanos');
+// buscarProducto('manzanas');
+
+ordenarProductosPorPrecio();
+
+// imprimirInventario();
+let array = ordenarProductosPorPrecio();
+imprimirInventario(array); 
